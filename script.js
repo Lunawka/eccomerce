fae8e3a10ad998aa18e71ef8dc96f5f1f9ec0a44
Lunawka <xiaomilunawka@gmail.com>
2024-05-01 21:57:39 +0300

    delete

--- script.js
+++ script.js
@@ -1,61 +1,91 @@
 const cardList = document.querySelector("#card-list");
+let deleteBtns = [];
+let deleteBtnsNew = [];
 
 const trunc = (text, maxLenght) =>
-    text?.length > maxLenght ? text?.substring(0, maxLenght - 3) + "..." : text;
+  text?.length > maxLenght ? text?.substring(0, maxLenght - 3) + "..." : text;
 
 const renderCards = (products) => {
-    products.forEach((product) => {
-        const cardHTML = ` 
-            <div class="card" style="width: 18rem; height: 280px">
-                <img src="${product.images[0]}" class="card-img-top" alt="${
-            product.title
-        }" />
+  products.forEach((product) => {
+    const imgs = product.images;
+    console.log(imgs[0]);
+    const cardHTML = ` 
+            <div class="card" style="width: 18rem; height: 330px">
+                <img src="header.png" class="card-img-top" alt="${
+      product.title
+    }" />
                 <div class="card-body">
-                    <h5 class="card-title">${product.title}</h5>
+                    <p class="card-id">Card №${product.id}</p>
+                    <h5 class="card-title">${
+                      product.title
+                    } </h5>                 
                     <p class="card-text">
                        ${trunc(product.description, 100)}
                     </p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <a href="#" id="deleteBtn" class="btn btn-danger">Delete</a>
                 </div>
             </div>`;
 
-        cardList.insertAdjacentHTML("beforeend", cardHTML);
-    });
+    cardList.insertAdjacentHTML("beforeend", cardHTML);
+    deleteBtnsNew.push(product.id);
+  });
+  deleteBtns = document.querySelectorAll("#deleteBtn");
+  deleteBtns.forEach((deleteBtn, key) => {
+    deleteBtn.addEventListener("click", () => {
+        deleteProduct(deleteBtnsNew[key]);
+      });
+  });
+  console.log(deleteBtns,deleteBtnsNew);
 };
 
-fetch("https://api.escuelajs.co/api/v1/products?offset=0&limit=10")
-    .then((response) => response.json())
-    .then((data) => {
-        renderCards(data);
-    });
+fetch("https://api.escuelajs.co/api/v1/products?offset=80&limit=10")
+  .then((response) => response.json())
+  .then((data) => {
+    renderCards(data);
+  });
 
 const addBtn = document.querySelector("#add-btn");
 const addProductForm = document.querySelector("#add-product-form");
 
 const addProduct = () => {
-    const title = document.querySelector("#product-title").value;
-    const description = document.querySelector("#product-descr").value;
-    const price = +document.querySelector("#product-price").value;
-    const categoryId = +document.querySelector("#product-category").value;
+  const title = document.querySelector("#product-title").value;
+  const description = document.querySelector("#product-descr").value;
+  const price = +document.querySelector("#product-price").value;
+  const categoryId = +document.querySelector("#product-category").value;
 
-    const productToAdd = {
-        title,
-        description,
-        price,
-        categoryId,
-        images: ["https://placeimg.com/640/480/any"],
-    };
+  const productToAdd = {
+    title,
+    description,
+    price,
+    categoryId,
+    images: ["https://placeimg.com/640/480/any"],
+  };
 
-    fetch("https://api.escuelajs.co/api/v1/products", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(productToAdd),
-    });
+  fetch("https://api.escuelajs.co/api/v1/products", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(productToAdd),
+  });
 };
 
 addProductForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    addProduct();
+  e.preventDefault();
+  addProduct();
 });
+
+
+const deleteProduct = (id) => {
+  fetch("https://api.escuelajs.co/api/v1/products/"+id, {
+    method: "DELETE",
+    headers: {
+      "accept": "*/*",
+    },
+  });
+  setTimeout(() => {
+    window.location.reload();
+  }, 500);
+};
+
